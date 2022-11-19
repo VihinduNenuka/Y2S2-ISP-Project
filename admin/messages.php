42babaf86aa7c12b660510dbbf66ea3587e7a71b
VihinduNenuka <118439092+VihinduNenuka@users.noreply.github.com>
2022-11-20 00:27:26 +0530

    Add files via upload

---
+++ admin/messages.php
@@ -0,0 +1,80 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+};
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_message = $conn->prepare("DELETE FROM `feedback` WHERE Username = ?");
+   $delete_message->execute([$Username]);
+   header('location:messages.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>messages</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="contacts">
+
+<h1 class="heading">messages</h1>
+
+<div class="box-container">
+
+   <?php
+      $select_messages = $conn->prepare("SELECT * FROM `feedback`");
+      $select_messages->execute();
+      if($select_messages->rowCount() > 0){
+         while($fetch_message = $select_messages->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <div class="box">
+   <p> name : <span><?= $fetch_message['Username']; ?></span></p>
+   <p> Rating  : <span><?= $fetch_message['Rating']; ?></span></p>
+   <p> Comment : <span><?= $fetch_message['Feedback']; ?></span></p>
+   <a href="messages.php??delete=<?= $fetch_message['Username']; ?>" onclick="return confirm('delete this message?');" class="delete-btn">delete</a>
+   </div>
+   <?php
+         }
+      }else{
+         echo '<p class="empty">you have no messages</p>';
+      }
+   ?>
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+
+<section class="footer">
+                <h4>Follow Us On</h4>
+                <div class="smicons">
+                    <a href="https://twitter.com/login"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+                </div>
+                <hr>
+                <div class="bottom">
+                        <div class="data">
+                            <p>2022 Salon HisaKes - All Rights Reserved.</p>
+                        </div>
+                        <div class="blink">
+                            <a href="">Terms of Use</a>
+                            <a href="">Data & Web Privacy Policies</a>
+                        </div>
+                </div>
+
+                <link rel="stylesheet" href="css/style.css">
+            </section>
+        </div>
+    </body>
+</html>
+</footer>
